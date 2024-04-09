b774cea0b59645b4bebee267ba754385ae9c0855
callum <callumgibbons@icloud.com>
2024-04-09 19:02:45 +0530

    changed container name so as not to intefere in the future. created an event to track the X and Y coordinates of the users mouse and converting them to change the styled position of the crosshair div present.

--- TS Game/src/main.ts
+++ TS Game/src/main.ts
@@ -1,8 +1,17 @@
 import "./style.css";
 
 const crosshair = document.querySelector("[data-crosshair]");
-const container = document.querySelector("#gameContainer");
+const gameContainer = document.querySelector("#gameContainer");
 
-if (!crosshair || !container) {
+if (!crosshair || !gameContainer) {
   throw new Error("Issue with selectors");
 }
+
+gameContainer.addEventListener("mousemove", function(event) {
+    const userX = event.clientX;
+    const userY = event.clientY;
+  
+    crosshair.style.transition = "0s";
+    crosshair.style.left = `${userX}px`;
+    crosshair.style.top = `${userY}px`;
+  });
