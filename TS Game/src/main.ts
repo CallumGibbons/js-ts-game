4c2fe89487aa7ebb863d9f521fa0e0c136ee3984
callum <callumgibbons@icloud.com>
2024-04-09 18:46:26 +0530

    created initial divs for game setup and added necessary selectors and non-selectors.

--- TS Game/src/main.ts
+++ TS Game/src/main.ts
@@ -1,2 +1,8 @@
 import "./style.css";
 
+const crosshair = document.querySelector("[data-crosshair]");
+const container = document.querySelector("#gameContainer");
+
+if (!crosshair || !container) {
+  throw new Error("Issue with selectors");
+}
