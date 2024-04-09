b9320c72b1906bdd77fe576ef32feb6ff3cb80a8
callum <callumgibbons@icloud.com>
2024-04-09 23:43:43 +0530

    added on moues out event to return the crosshair to the centre of the game div when the users mouse leaves the area with a transition to make it look more appealing.

--- TS Game/src/main.ts
+++ TS Game/src/main.ts
@@ -8,10 +8,15 @@ if (!crosshair || !gameContainer) {
 }
 
 gameContainer.addEventListener("mousemove", function(event) {
-    const userX = event.clientX;
-    const userY = event.clientY;
-  
-    crosshair.style.transition = "0s";
-    crosshair.style.left = `${userX}px`;
-    crosshair.style.top = `${userY}px`;
-  });
+  const userX = event.clientX;
+  const userY = event.clientY;
+
+  crosshair.style.transition = "0s";
+  crosshair.style.left = `${userX}px`;
+  crosshair.style.top = `${userY}px`;
+});
+document.onmouseout = (event) => {
+  crosshair.style.transition = "1s";
+  crosshair.style.left = "50%";
+  crosshair.style.top = "50%";
+};
