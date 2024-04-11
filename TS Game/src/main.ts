d5ce2c00e185d6373255ae56c411e3145e959831
callum <callumgibbons@icloud.com>
2024-04-11 22:17:31 +0530

    set up functions for tracking shots and increasing score on target hit.

--- TS Game/src/main.ts
+++ TS Game/src/main.ts
@@ -4,33 +4,39 @@ let score: number = 0;
 let shotsHit: number = 0;
 let shotsMissed: number = 0;
 let currentScore: number = 0;
-let targetScore: number = 0;
+let targetsHit: number = 0;
+let targetScore: number = 150;
 let totalShots: number = 0;
 let accuracy: number = (shotsMissed / totalShots) * 100;
 let roundTimer: number = 30;
 let dificutyScale: number = 1;
 
-const start = document.querySelector(".gameButtonStart")
+const target = document.querySelector<HTMLDivElement>(".target");
 const gunshot = document.querySelector("audio");
 const crosshair = document.querySelector<HTMLDataElement>("[data-crosshair]");
 const gameContainer = document.querySelector<HTMLDivElement>("#gameContainer");
 
-if (!crosshair || !gameContainer || !gunshot) {
+if (!crosshair || !gameContainer || !gunshot || !target) {
   throw new Error("Issue with selectors");
 }
 
+target.addEventListener("click", (click: MouseEvent) => {
+  currentScore = currentScore + targetScore;
+  console.log(currentScore);
+});
 
-
-gameContainer.addEventListener("click", click => {
+gameContainer.addEventListener("click", (click: MouseEvent) => {
+  totalShots = totalShots + 1;
   gunshot.volume = 0.2;
   gunshot.currentTime = 0;
   gunshot.play();
+  console.log(totalShots);
 });
 
 gameContainer.addEventListener("mousemove", function (event: MouseEvent) {
   const userX = event.clientX;
   const userY = event.clientY;
-console.log(event);
+  console.log(event);
 
   crosshair.style.transition = "0s";
   crosshair.style.left = `${userX}px`;
