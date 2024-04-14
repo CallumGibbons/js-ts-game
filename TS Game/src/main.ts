fe37df15ea119c51a1abed96803dc66607b01b87
callum <callumgibbons@icloud.com>
2024-04-14 20:42:01 +0530

    Timer successfully created and errors for non-called click events quelled.

--- TS Game/src/main.ts
+++ TS Game/src/main.ts
@@ -2,7 +2,6 @@ import "./style.css";
 
 let roundCounter = 0;
 let gameState = 0;
-const startBlinkTime = 1000;
 let score: number = 0;
 let shotsHit: number = 0;
 let totalShots: number = 0;
@@ -13,11 +12,14 @@ let targetScore: number = 150;
 let accuracy: number = (shotsMissed / totalShots) * 100;
 let roundTimer: number = 30;
 let difficultyScale: number = 1;
+let timeLeft = 30;
 
-const timerCounter = document.querySelector<HTMLParagraphElement>("#timer");
+const timerCounter = document.querySelector("#timer") as HTMLElement;
 const startText = document.querySelector<HTMLDivElement>("#gameText--Start");
-const startButton = document.querySelector<HTMLButtonElement>("#gameButtonStart");
-const resetButton = document.querySelector<HTMLButtonElement>("#gameButtonReset");
+const startButton =
+  document.querySelector<HTMLButtonElement>("#gameButtonStart");
+const resetButton =
+  document.querySelector<HTMLButtonElement>("#gameButtonReset");
 const target = document.querySelector<HTMLDivElement>(".target");
 const gunshot = document.querySelector<HTMLAudioElement>("audio");
 const crosshair = document.querySelector<HTMLDataElement>("[data-crosshair]");
@@ -31,18 +33,31 @@ if (
   !startText ||
   !startButton ||
   !timerCounter ||
-  !timer ||
   !resetButton
 ) {
   throw new Error("Issue with selectors");
 }
 
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == -1) {
+    clearTimeout(timerId);
+  } else {
+    timerCounter.innerHTML = timeLeft + " seconds remaining";
+    timeLeft--;
+  }
+}
+
 startButton.addEventListener("click", (click: MouseEvent) => {
   gameState += 1;
   if (gameState == 1) {
-    target.style.display = "inherit";
     startText.style.display = "inherit";
-    setTimeout(function() { startText.style.display="none"; }, 3000)
+    setTimeout(function () {
+      startText.style.display = "none";
+    }, 3000);
+    target.style.display = "inherit";
+    countdown()
   } else {
     totalShots == 0;
     gameState == 0;
@@ -51,18 +66,7 @@ startButton.addEventListener("click", (click: MouseEvent) => {
   console.log(gameState);
 });
 
-function timer() {
-  var sec = 30;
-  var timer = setInterval(function () {
-    document.getElementById('timer').innerHTML = "00:" + sec;
-    sec--;
-    if (sec < 0) {
-      clearInterval(timer);
-    }
-  }, 1000);
-}
-
-target.addEventListener("click", (click: MouseEvent) => {
+target.addEventListener("click", (_click: MouseEvent) => {
   targetsHit += 1;
   shotsHit += 1;
   currentScore = currentScore + targetScore;
@@ -70,7 +74,7 @@ target.addEventListener("click", (click: MouseEvent) => {
   console.log(targetsHit);
 });
 
-gameContainer.addEventListener("click", (click: MouseEvent) => {
+gameContainer.addEventListener("click", (_click: MouseEvent) => {
   totalShots = totalShots + 1;
   gunshot.volume = 0.2;
   gunshot.currentTime = 0;
@@ -93,7 +97,6 @@ document.onmouseout = (event) => {
   crosshair.style.top = "50%";
 };
 
-
 resetButton.addEventListener("click", (click: MouseEvent) => {
   roundCounter = 0;
   gameState = 0;
@@ -108,4 +111,4 @@ resetButton.addEventListener("click", (click: MouseEvent) => {
   roundTimer = 30;
   difficultyScale = 1;
   target.style.display = "none";
-})
+});
